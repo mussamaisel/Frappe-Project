5b5f7bbc757ec110d8de240633d6aa661071e8fa
Mussa Maisel <mussamaisel@gmail.com>
2026-09-21 15:58:15 +0300

    Reverted institution self registration to admin-only desk creation; added geolocation fields; configured and verified SMTP

--- blood_bank/blood_bank/doctype/blood_institution/blood_institution.py
+++ blood_bank/blood_bank/doctype/blood_institution/blood_institution.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2026, JACKSON ANDREW and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.utils.nestedset import NestedSet
 
 
--- blood_bank/utils.py
+++ blood_bank/utils.py
@@ -2,6 +2,9 @@ import frappe
 
 
 def assign_donor_role(doc, method=None):
+    if frappe.session.user != "Guest":
+        return
+
     if doc.user_type == "Website User":
         if "Donator" not in [role.role for role in doc.roles]:
             doc.append("roles", {"role": "Donator"})
